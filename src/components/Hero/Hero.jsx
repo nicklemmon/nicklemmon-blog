bd4ff9539717571c54233bc494c0987997561914
Nick Lemmon <nick.lemmon@gmail.com>
2018-09-02 12:18:01 -0400

    Add additional header components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,16 +4,19 @@ import Container from '../Container/Container'
 import Button from '../Button/Button'
 
 import './Hero.css'
+import heroImage from './hero-bg.png'
 
 export default class Hero extends React.Component {
   render() {
     return (
       <div className='Hero'>
-        <Container size='xl'>
+        <Container className='Hero-container' size='xxl'>
           <h1 className='Hero-title'>A front end developer driven to create accessible&nbsp;user interfaces</h1>
 
           <Button type='ghost' to='/contact'>Get in Touch</Button>
         </Container>
+
+        <img className='Hero-image' src={ heroImage }/>
       </div>
     )
   }
