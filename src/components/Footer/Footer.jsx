90883f7ebc1a0f1232c70d20898b984615056c32
Nick Lemmon <nick.lemmon@gmail.com>
2018-09-03 19:21:34 -0400

    Add overflowing pseudo element below the breakout container

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className='Footer'>
-        <Container>
+        <Container size='lg'>
           <p>All rights reserved, <strong>Nick Lemmon</strong> { new Date().getFullYear() }</p>
         </Container>
       </footer>
