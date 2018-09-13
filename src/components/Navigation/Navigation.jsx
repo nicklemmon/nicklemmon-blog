0a2de17cff7b61f9e34291b99043b64f13f9e789
Nick Lemmon <nick.lemmon@gmail.com>
2018-09-12 22:18:34 -0400

    Keyboard trap works for nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -16,6 +16,28 @@ export default class Navigation extends React.Component {
     }
 
     this.handleClick = this.handleClick.bind( this )
+    this.handleStartKeypress = this.handleStartKeypress.bind( this )
+    this.handleEndKeypress = this.handleEndKeypress.bind( this )
+
+    this.firstFocusable = React.createRef()
+    this.lastFocusable = React.createRef()
+  }
+
+  handleStartKeypress( e ) {
+    if ( e.shiftKey && e.keyCode === 9 ) {
+      e.preventDefault()
+
+      console.log( this.lastFocusable )
+      this.lastFocusable.focus()
+    }
+  }
+
+  handleEndKeypress( e ) {
+    if ( !e.shiftKey && e.keyCode === 9 ) {
+      e.preventDefault()
+
+      this.firstFocusable.current.focus()
+    }
   }
 
   handleClick( e ) {
@@ -44,6 +66,8 @@ export default class Navigation extends React.Component {
           aria-expanded={ isOpen } 
           aria-controls='nav-menu'
           onClick={ this.handleClick }
+          onKeyDown={ ( e ) => this.handleStartKeypress( e ) }
+          ref={ this.firstFocusable }
         >
           <ScreenReaderOnly>Menu</ScreenReaderOnly>
 
@@ -60,7 +84,12 @@ export default class Navigation extends React.Component {
             <Highlight>About</Highlight>
           </Link>
 
-          <Link className='Navigation-link' to='/contact'>
+          <Link
+            className='Navigation-link'
+            to='/contact'
+            onKeyDown={ ( e ) => this.handleEndKeypress( e ) }
+            innerRef={ ( el ) => { this.lastFocusable = el } }
+          >
             <Highlight>Contact</Highlight>
           </Link>
         </nav>
