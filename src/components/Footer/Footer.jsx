834c208856700eb7521395a60da97d2350dd9003
Nick Lemmon <nick.lemmon@gmail.com>
2018-09-16 13:36:23 -0400

    Add social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Icon } from 'react-icons-kit'
+import { socialCodepen } from 'react-icons-kit/ionicons/socialCodepen'
+import { socialGithub } from 'react-icons-kit/ionicons/socialGithub'
+import { socialLinkedin } from 'react-icons-kit/ionicons/socialLinkedin'
+
 
 import Container from '../Container/Container'
 
@@ -9,6 +14,32 @@ export default class Footer extends React.Component {
     return (
       <footer className='Footer'>
         <Container size='xl'>
+          <div className='Footer-links'>
+            <a className='Footer-iconLink' href='http://codepen.com/nicklemmon' rel='noopener'>
+              <Icon
+                className='Footer-icon'
+                size={ '2rem' }
+                icon={ socialCodepen }
+              />
+            </a>
+
+            <a className='Footer-iconLink' href='http://github.com/nicklemmon' rel='noopener'>
+              <Icon
+                className='Footer-icon'
+                size={ '2rem' }
+                icon={ socialGithub }
+              />
+            </a>
+
+            <a className='Footer-iconLink' href='http://linkedin.com/in/nicklemmon' rel='noopener'>
+              <Icon
+                className='Footer-icon'
+                size={ '2rem' }
+                icon={ socialLinkedin }
+              />
+            </a>
+          </div>
+
           <p>All rights reserved, <strong>Nick Lemmon</strong> { new Date().getFullYear() }</p>
         </Container>
       </footer>
