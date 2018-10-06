36aac2d47f155ffcba6b03898333e03a64526280
Nick Lemmon <nick.lemmon@gmail.com>
2018-10-06 11:12:37 -0400

    Fix issues with blog posts not rendering

---
+++ src/components/Bio/Bio.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Bio extends React.Component {
+  render() {
+    return <p>A bio.</p>
+  }
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -24,6 +24,7 @@ class BlogPostTemplate extends React.Component {
         </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr/>
+        
         <Bio />
 
         <ul
