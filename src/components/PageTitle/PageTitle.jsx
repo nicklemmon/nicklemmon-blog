55acd4c476e5d2eebd9d04b67ae6d475771b262f
Nick Lemmon <nick.lemmon@gmail.com>
2018-11-11 18:28:59 -0500

    Managing focus for PageTitle component

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -10,6 +10,16 @@ import './PageTitle.css'
 import pageBackgroundImage from './page-bg.png'
 
 export default class PageTitle extends React.Component {
+  constructor( props ) {
+    super( props )
+
+    this.title = React.createRef()
+  }
+
+  componentDidMount() {
+    this.title.current.focus()
+  }
+
   render() {
     const {
       className,
@@ -20,11 +30,13 @@ export default class PageTitle extends React.Component {
     return (
       <Container className={ classNames( 'PageTitle', className ) } size='xl'>
         <MaxWidth className='PageTitle-maxWidth' size='lg'>
-          <Heading className='PageTitle-heading' level='1'>
-            <Highlight>
-              { headingContent }
-            </Highlight>
-          </Heading>
+          <div tabIndex='-1' ref={ this.title }>
+            <Heading className='PageTitle-heading' level='1'>
+              <Highlight>
+                { headingContent }
+              </Highlight>
+            </Heading>
+          </div>
 
           { date && 
             <div className='PageTitle-meta'>
